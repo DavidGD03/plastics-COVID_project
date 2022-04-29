b19430359bcf86fa248b609979bf45964b30200a
David Rojas <71358878+DavidGD03@users.noreply.github.com>
2022-04-29 12:19:00 -0500

    Fix variable name

--- scripts/generatePredictions.py
+++ scripts/generatePredictions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import model_selection
 from sklearn.metrics import accuracy_score
 
 from sklearn.model_selection import train_test_split
@@ -211,7 +212,7 @@ def main():
     region = sys.argv[1]
     modelo = sys.argv[2]
 
-    if modelo == 'LSTM' or model== '':
+    if modelo == 'LSTM' or modelo== '':
         
         # define model
         model = Sequential()
