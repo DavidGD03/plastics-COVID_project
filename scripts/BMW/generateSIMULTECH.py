ba4e8959a0047f990c92120da9ab3000804bc572
David <davidrojasuis20@gmail.com>
2022-07-24 16:37:23 -0500

    Arreglar errores y eliminar código innecesario

--- scripts/BMW/generateSIMULTECH.py
+++ scripts/BMW/generateSIMULTECH.py
@@ -3,17 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
-import sys
 import os
-import sys
 from sklearn.preprocessing import MinMaxScaler
 import datetime
-from sklearn.metrics import mean_squared_error
 
 # Parámetros de entrada del script
-region = sys.argv[1]
-modelo = sys.argv[2]
-window = sys.argv[3]
+region = 'AMB'
+modelo = 'LSTM'
+window = '7'
 
 absolutepath = os.path.abspath(__file__)
 fileDirectory = os.path.dirname(absolutepath)
@@ -22,49 +19,37 @@ resultspath=fileDirectory+'\\results\\'+modelo +'\\'+region # Carpeta con las gr
 
 def generar_train_test_datasets(escenario,X_scaler,Y_scaler):
     dataURLRegiones='https://raw.githubusercontent.com/DavidGD03/plastics-COVID_project/main/data/India_5_Regiones_Simultech3/'
-    dataURLSEIRD='https://raw.githubusercontent.com/DavidGD03/plastics-COVID_project/main/data/SEIRD_AMB/'
+    # dataURLSEIRD='https://raw.githubusercontent.com/DavidGD03/plastics-COVID_project/main/data/SEIRD_AMB_Paper_Old/'
+    # dataURLSEIRD='https://raw.githubusercontent.com/DavidGD03/plastics-COVID_project/main/data/SEIRD_AMB_Paper/'
+    dataURLSEIRD='https://raw.githubusercontent.com/DavidGD03/plastics-COVID_project/main/data/SEIRD_AMBv4/'
     bmw_dataS=pd.read_excel('https://raw.githubusercontent.com/DavidGD03/plastics-COVID_project/main/data/india/total_bmw_waste.xlsx?raw=true',sheet_name=2)
     bmw_dataS['FECHA'] = pd.to_datetime(bmw_dataS['FECHA'], infer_datetime_format=True)
     bmw_dataS=bmw_dataS.fillna(bmw_dataS.mean())
     bmw_dataS=bmw_dataS.set_index('FECHA')
-    if region == 'AMB':
-        df_multivariable=pd.read_csv(dataURLRegiones+'df_multivariable_AMB.csv')
+    df_multivariable=pd.read_csv(dataURLRegiones+'df_multivariable_AMB.csv')
+    df_multivariable=df_multivariable.set_index('FECHA')
     if escenario == 'pessimistic':
-        df_IPesimista=pd.read_csv(dataURLSEIRD+'ISEIRDPESIMISTA.csv')
-        df_DPesimista=pd.read_csv(dataURLSEIRD+'DSEIRDPESIMISTA.csv')
+        df_IPesimista=pd.read_csv(dataURLSEIRD+'ISEIRDPESIMISTA.csv',names=['FECHA','AMB'],header=0, index_col='FECHA')
+        df_DPesimista=pd.read_csv(dataURLSEIRD+'DSEIRDPESIMISTA.csv',names=['FECHA','AMB'],header=0, index_col='FECHA')
         df_multivariable['Casos']=df_IPesimista['AMB']
         df_multivariable['Muertes']=df_DPesimista['AMB']
-        # df_multivariable['Casos']=3
-        # df_multivariable['Muertes']=3
     elif escenario == 'neutral':
-        df_INeutral=pd.read_csv(dataURLSEIRD+'ISEIRDNEUTRAL.csv')
-        df_DNeutral=pd.read_csv(dataURLSEIRD+'DSEIRDNEUTRAL.csv')
+        df_INeutral=pd.read_csv(dataURLSEIRD+'ISEIRDNEUTRAL.csv',names=['FECHA','AMB'],header=0, index_col='FECHA')
+        df_DNeutral=pd.read_csv(dataURLSEIRD+'DSEIRDNEUTRAL.csv',names=['FECHA','AMB'],header=0, index_col='FECHA')
         df_multivariable['Casos']=df_INeutral['AMB']
         df_multivariable['Muertes']=df_DNeutral['AMB']
-        # df_multivariable['Casos']=2
-        # df_multivariable['Muertes']=2
     elif escenario == 'optimistic':
-        df_IOptimista=pd.read_csv(dataURLSEIRD+'ISEIRDOPTIMISTA.csv')
-        df_DOptimista=pd.read_csv(dataURLSEIRD+'DSEIRDOPTIMISTA.csv')
+        df_IOptimista=pd.read_csv(dataURLSEIRD+'ISEIRDOPTIMISTA.csv',names=['FECHA','AMB'],header=0, index_col='FECHA')
+        df_DOptimista=pd.read_csv(dataURLSEIRD+'DSEIRDOPTIMISTA.csv',names=['FECHA','AMB'],header=0, index_col='FECHA')
         df_multivariable['Casos']=df_IOptimista['AMB']
         df_multivariable['Muertes']=df_DOptimista['AMB']
-        # df_multivariable['Casos']=1
-        # df_multivariable['Muertes']=1
-
-    df_multivariable['FECHA'] = pd.to_datetime(df_multivariable['FECHA'], infer_datetime_format=True)
-    df_multivariable=df_multivariable.set_index('FECHA')
-    # X_scaler = MinMaxScaler()
-    # Y_scaler = MinMaxScaler()
-    # X_data=X_scaler.fit_transform(df_multivariable[['Casos', 'Muertes', 'Mov Residencial', 'Mov trabajo', 'Mov estaciones']])
-    # Y_data = Y_scaler.fit_transform(df_multivariable[['BMW']])
+    df_multivariable.index = pd.to_datetime(df_multivariable.index, infer_datetime_format=True)
     X_data=X_scaler.transform(df_multivariable[['Casos', 'Muertes', 'Mov Residencial', 'Mov trabajo', 'Mov estaciones']])
     Y_data = Y_scaler.transform(df_multivariable[['BMW']])
     df_multivariable[['Casos', 'Muertes', 'Mov Residencial', 'Mov trabajo', 'Mov estaciones']]=X_data
     df_multivariable[['BMW']]=Y_data
-
     train_MRNN_sc=df_multivariable['2020-06-17':'2021-08-31']
     test_MRNN_sc=df_multivariable['2021-09-01':]
-	
     test_MRNN_sc['BMW'] = 0    #Unicamente para asegurar que las predicciones no esten tomando los valores reales de BMW 
     return train_MRNN_sc, test_MRNN_sc, Y_scaler
 
@@ -82,7 +67,6 @@ def generar_predicciones(model,train_MRNN_scN,test_MRNN_scN,Y_scaler,n_input,n_f
     first_eval_batch = train_MRNN_scN[-n_input:]
     current_batch = first_eval_batch.reshape((1, n_input, n_features))
     test_batch= test_variables.reshape((1,len(test_variables),n_features))
-
     for i in range(len(test_MRNN_scN)):
         # get the prediction value for the first batch
         current_pred = model.predict(current_batch)[0]
@@ -92,23 +76,19 @@ def generar_predicciones(model,train_MRNN_scN,test_MRNN_scN,Y_scaler,n_input,n_f
         temporal= test_batch[0,i,:]
         temporal= temporal.reshape((1,1,6))
         # use the prediction to update the batch and remove the first value
-        #current_batch = np.append(current_batch[:,1:,:],[[current_pred]],axis=1)
         current_batch = np.append(current_batch[:,1:,:],temporal,axis=1)
-    
     true_predictions = Y_scaler.inverse_transform([test_predictions])
     Predicciones=[]
-
     for i in range(len(test_MRNN_scN)):
         Predicciones.append(true_predictions[0][i])
-
     return Predicciones
 
-def plot_predicciones(testinverse, n_input, lista_escenarios):
+def plot_predicciones(testinverse, n_input, lista_escenarios, df_real_data):
     fig = plt.figure(figsize=(10, 6))
     for escenario in lista_escenarios:
         plt.plot(testinverse.index,testinverse['predictions_' + escenario],label="Predictions for the " + escenario + " scenario")
     plt.axvline(x=datetime.date(2021, 9, 1), ymin=-1, ymax=2,color="black",linestyle = "dashed",label="Start of the forecasting")
-
+    plt.plot(df_real_data.index,df_real_data['BMW'], label="Real data")
     plt.legend(loc='best')
     plt.title("Real predictions using the "+modelo + " model and a window-size of "+str(n_input)+ " for " + region)
     plt.xlabel("Date")
@@ -117,7 +97,6 @@ def plot_predicciones(testinverse, n_input, lista_escenarios):
 
 def main():
     dataURLRegiones='https://raw.githubusercontent.com/DavidGD03/plastics-COVID_project/main/data/India_5_Regiones_Simultech3/'
-    dataURLSEIRD='https://raw.githubusercontent.com/DavidGD03/plastics-COVID_project/main/data/SEIRD_AMB/'
     bmw_dataS=pd.read_excel('https://raw.githubusercontent.com/DavidGD03/plastics-COVID_project/main/data/india/total_bmw_waste.xlsx?raw=true',sheet_name=2)
     bmw_dataS['FECHA'] = pd.to_datetime(bmw_dataS['FECHA'], infer_datetime_format=True)
     bmw_dataS=bmw_dataS.fillna(bmw_dataS.mean())
@@ -133,15 +112,12 @@ def main():
     for escenario in lista_escenarios:
         train_MRNN_sc, test_MRNN_sc, Y_scaler=generar_train_test_datasets(escenario,X_scaler,Y_scaler)
         n_input,n_features,train_MRNN_scN, test_MRNN_scN=generador_serie_tiempo(train_MRNN_sc,test_MRNN_sc)
-
         model = tf.keras.models.load_model(fileDirectory + '/LSTM_model.h5')
         Predicciones=generar_predicciones(model,train_MRNN_scN,test_MRNN_scN,Y_scaler,n_input,n_features)
-
         testinverse['predictions_' + escenario]=Predicciones
 
     # Create subfolder for results
     isExist = os.path.exists(resultspath)
-
     if isExist != True:
         try:
             os.makedirs(resultspath)
@@ -151,7 +127,11 @@ def main():
         else:
             print("Se ha creado el directorio correctamente ")
 
-    plot_predicciones(testinverse,n_input, lista_escenarios)
+    dataURLRegiones='https://raw.githubusercontent.com/DavidGD03/plastics-COVID_project/main/data/India_5_Regiones_Simultech3/'
+    df_real_data=pd.read_csv(dataURLRegiones+'df_multivariable_AMB.csv')
+    df_real_data['FECHA'] = pd.to_datetime(df_real_data['FECHA'], infer_datetime_format=True)
+    df_real_data=df_real_data.set_index('FECHA')
+    plot_predicciones(testinverse,n_input, lista_escenarios, df_real_data)
     return 0
 
 if __name__ == '__main__':
